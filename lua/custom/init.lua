85bf98a9f018b61cb24beac6d72d77979ab222db
roey ben arieh <roey280404@gmail.com>
2023-09-16 13:26:20 +0300

    feat: english spell cheking

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,2 +1,12 @@
-# this file is used to override neovim options and commands
+-- this file is used to override neovim options and commands
+
+-- relative line numbers
 vim.wo.relativenumber = true
+
+-- spelling documentation: 
+-- https://neovim.io/doc/user/spell.html
+-- https://jdhao.github.io/2019/04/29/nvim_spell_check/
+-- https://johncodes.com/posts/2023/02-25-nvim-spell/
+-- english spell checks 
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
