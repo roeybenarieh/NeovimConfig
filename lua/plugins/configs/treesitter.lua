d9194171d5a9348f3063c5272920a5b58712b5d8
roey ben arieh <roey280404@gmail.com>
2023-10-06 21:35:22 +0300

    feat(nvim-treesitter): more languages are installed, and adding incremental selection

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,6 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = { "lua", "python", "bash", "go", "markdown", "rust", "cpp", "c_sharp", 
+                      "javascript", "typescript", "sql", "promql", "regex", "html", "css", "json", "yaml" },
 
   highlight = {
     enable = true,
@@ -7,6 +8,16 @@ local options = {
   },
 
   indent = { enable = true },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<C-space>',
+      node_incremental = '<C-space>',
+      scope_incremental = '<C-s>',
+      node_decremental = '<C-backspace>',
+    },
+  },
 }
 
 return options
